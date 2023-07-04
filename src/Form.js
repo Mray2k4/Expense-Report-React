6f5d4fa9829d3f55697628c4baf8615091e25031
Marque Ray <mray2k4@yahoo.com>
2023-07-04 00:49:08 -0700

    changed code in Tables.js to  Forms.js

--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,38 @@
+import {useState} from 'react';
 
 function Form() {
+    const [date, setDate] = useState('')
+    const [description, setDescription] = useState('')
+    const [category, setCategory] = useState('')
+    const [amount, setAmount] = useState(0)
+
+
     return (  
         <form className="main">
-            <div id="data">
-                <input type="date" id="date" placeholder='date'/>
-                <input type="text" id="descript" placeholder='Description'/>
-                <input type="text" id="category" placeholder='Category'/>
-                <input type="number" id="amount" placeholder='Amount'/>
+                <input
+                  onChange={e => setDate(e.target.value)} 
+                  type="date"
+                  id="date" 
+                  placeholder='date'/>
+                <input 
+                  onChange={e => setDescription(e.target.value)}
+                  type="text" 
+                  id="descript" 
+                  placeholder='Description'
+                />
+                <input 
+                  onChange={e => setCategory(e.target.value)}
+                  type="text" 
+                  id="category" 
+                  placeholder='Category'
+                />
+                <input 
+                  onChange={e => setAmount(e.target.value)}
+                  type="number" 
+                  id="amount" 
+                  placeholder='Amount'
+                />
                 <button id='btnAdd'>Add</button>
-            </div>
         </form>
     );
 }
--- src/Table.js
+++ src/Table.js
@@ -1,21 +1,23 @@
 import {useState} from 'react';
 
 function Table() {
-    const [date, setDate] = useState()
-    const [description, setDescription] = useState()
-    const [category, setCategory] = useState()
-    const [amount, setAmmount] = useState()
+    const [date, setDate] = useState('')
+    const [description, setDescription] = useState('')
+    const [category, setCategory] = useState('')
+    const [amount, setAmmount] = useState(0)
+
+    const form = `<tr>
+                    <td>${date}</td>
+                    <td>${description}</td>
+                    <td>${category}</td>
+                    <td>${$ + amount}</td>
+                    <td><input type="button" value="Delete" onclick="deleteRow(this)"/></td>
+                </tr>`;
 
     return (
-                <div>
-                 <tr>
-                  <td>{date}</td>
-                  <td>{description}</td>
-                  <td>{category}</td>
-                  <td>{$ + amount}</td>
-                  <td><input type="button" value="Delete" onclick="deleteRow(this)"/></td>
-                 </tr>;
-                </div>
+         <>
+         {form}
+         </>       
     )
 }
 
